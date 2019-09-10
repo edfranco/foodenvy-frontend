cfeeacd468ed4d11cb406a86cc00d73e8cd919bf
edfranco <edfranco415@gmail.com>
2019-09-10 16:13:48 -0700

    Updates

---
+++ precache-manifest.b39488a219abe37bbc6ace7ce3ea06c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68d30b3709e3f5d8f15dc9ad2c22e0c",
+    "url": "/foodenvy-frontend/index.html"
+  },
+  {
+    "revision": "cfbf42947734b2ea85fa",
+    "url": "/foodenvy-frontend/static/css/main.0ae7cc0d.chunk.css"
+  },
+  {
+    "revision": "f23f8741365cf500e755",
+    "url": "/foodenvy-frontend/static/js/2.72b95fee.chunk.js"
+  },
+  {
+    "revision": "cfbf42947734b2ea85fa",
+    "url": "/foodenvy-frontend/static/js/main.cac98e85.chunk.js"
+  },
+  {
+    "revision": "c991445c2ffc3fcee025",
+    "url": "/foodenvy-frontend/static/js/runtime~main.49d9aba2.js"
+  }
+]);
